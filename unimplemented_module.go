7a1b6356eebdde5ee821693cecc2cc454ffa2954
Tit Petric <black@titpetric.com>
2025-10-30 19:38:55 +0100

    Add UnimplementedModule to help with module development

---
+++ unimplemented_module.go
@@ -0,0 +1,27 @@
+package platform
+
+// UnimplementedModule implements the module contract.
+// The module can embed the type to skip implementing
+// any of the bound functions.
+type UnimplementedModule struct{}
+
+// Name returns an empty string.
+func (UnimplementedModule) Name() string {
+	return ""
+}
+
+// Start returns nil (no error).
+func (UnimplementedModule) Start() error {
+	return nil
+}
+
+// Stop returns nil (no error).
+func (UnimplementedModule) Stop() error {
+	return nil
+}
+
+// Mount does nothing.
+func (UnimplementedModule) Mount(Router) {
+}
+
+var _ Module = UnimplementedModule{}
