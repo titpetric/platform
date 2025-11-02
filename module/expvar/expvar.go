accd0911f229ab763ad5152379d18b2d274e22c7
Tit Petric <black@titpetric.com>
2025-11-02 14:30:24 +0100

    Adjust uptime registration in module/expvar

--- module/expvar/expvar.go
+++ module/expvar/expvar.go
@@ -15,13 +15,17 @@ func NewHandler() *Handler {
 	return &Handler{}
 }
 
+func (m *Handler) Start() error {
+	start := time.Now()
+	if expvar.Get("uptime") == nil {
+		expvar.Publish("uptime", expvar.Func(func() interface{} {
+			return time.Since(start).Seconds()
+		}))
+	}
+	return nil
+}
+
 func (m *Handler) Mount(r platform.Router) error {
 	r.Mount("/debug/vars", expvar.Handler())
-
-	start := time.Now()
-	expvar.Publish("uptime", expvar.Func(func() interface{} {
-		return time.Since(start).Seconds()
-	}))
-
 	return nil
 }
